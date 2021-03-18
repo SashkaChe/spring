011b9917572990d170606152086354220e5930d6
Alexsandr Chevtaev <alex-chevtaev@yandex.ru>
2021-03-18 08:09:09 +0300

    Ещё один апдэйт

--- spring-app/src/main/java/ru/neutrino/Country.java
+++ spring-app/src/main/java/ru/neutrino/Country.java
@@ -1,23 +1,44 @@
 package ru.neutrino;
 
-import org.springframework.stereotype.Component;
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import lombok.Getter;
 import lombok.Setter;
 
-@Component() 
-@Getter
+@EnableTransactionManagement
+@Entity
+@Table(name="Country")
 @Setter
-public class Country {
+public class CountryJPA implements Serializable {
 
 	
-	private int id;
+	private long id;
 	private String country;
-
 	
+	
+	@Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId () { 
+		return id; 
+	}	
+	
+	
+	@Column(name="country")
+	public String getCountry () { 
+		return country; 
+	}
+
+
 	@Override
 	public String toString() {
-		return "Country [id=" + id + ", country=" + country + "]";
-	}
+		return "CountryJPA [id=" + id + ", country=" + country + "]";
+	}	
+	
+	
+	
 }
-
